45d79b29adcd71144d361d313e1f41ce58de5c0c
shaktigurung <mr.shakti.gurung@gmail.com>
2019-05-28 08:59:20 +1000

    get and put lambda functions

--- getUserData/index.js
+++ getUserData/index.js
@@ -3,19 +3,36 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: "ap-southeast-2"})
 exports.handler = async (event, context ) => {
     const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region: "ap-southeast-2"})
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "ap-southeast-2"});
+
+    let responseBody = " ";
+    let statusCode = 0;
+
+    const {id} = event.pathParameters;
    
     const params = {
         TableName: "Users",
         Key: {
-            id:  "12345"
+            id:  id
         }
     }
 
     try {
         const data = await  documentClient.get(params).promise();
-        console.log(data);
+        responseBody = JSON.stringify(data.Item);
+        statusCode = 200;
     } catch (err) {
-        console.log(err);
-    }       
+        responseBody = `Unable to get response data`;
+        statusCode = 403;
+    }   
+    
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myHeader": "test"
+        },
+        body: responseBody
+    }
+
+    return response;
 }
--- putUserData/index.js
+++ putUserData/index.js
@@ -5,19 +5,35 @@ exports.handler = async (event, context ) => {
     const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     const documentClient = new AWS.DynamoDB.DocumentClient({region: "ap-southeast-2"})
    
+    let responseBody = " ";
+    let statusCode = 0;
+
+    const {id, firstname, lastname} = JSON.parse(event.body);
     const params = {
         TableName: "Users",
         Item: {
-            id:  "12345",
-            firstname: "John",
-            lastname: "Doe"
+            id: id,
+            firstname: firstname,
+            lastname: lastname
         }
     }
 
     try {
         const data = await  documentClient.put(params).promise();
-        console.log(data);
+        responseBody = JSON.stringify(data.Item);
+        statusCode = 201;
     } catch (err) {
-        console.log(err);
-    }       
+        responseBody = `Unable to put response data`;
+        statusCode = 403;
+    }   
+    
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myHeader": "test"
+        },
+        body: responseBody
+    }
+
+    return response;
 }
